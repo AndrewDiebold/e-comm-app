66bde3cb899fca484ebcdbb0ce5d4e0db0a4ff4b
AndrewDiebold <andrewdiebold@icloud.com>
2021-05-04 20:39:37 -0500

    added header component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-
 import React from 'react';
-import { Route } from 'react-router-dom';
-
-import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
